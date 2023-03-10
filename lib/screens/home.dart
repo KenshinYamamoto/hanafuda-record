dff66bd5c9f7c8ee4e69d41fb305bc2154882de1
山本　堅心 <s2032149nb@s.chibakoudai.jp>
2023-03-10 14:25:56 +0900

    Firebaseとの接続を行い、googleAuthでLoginできることを確認した

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    debugPrint(FirebaseAuth.instance.currentUser!.displayName);
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomePage'),
